8d41aef2e3c7e98aa8ac592ac25ce95cb86e0b54
Pawel Buderaski <pbuderaski@gmail.com>
2020-01-31 16:01:31 +0000

    feat: added orientation handler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import { sendQuickBrickEvent } from "@applicaster/zapp-react-native-bridge/QuickBrick";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,13 +15,15 @@ const styles = StyleSheet.create({
     fontSize: 25
   }
 });
-
 const App = () => {
+  sendQuickBrickEvent("allowedOrientationsForScreen", {
+    orientation: 1
+  });
+  console.log("it works!");
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 };
-
 export default App;

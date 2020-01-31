2a13bface2953332ecb5c0d2e96c0230cb368fe4
Pawel Buderaski <pbuderaski@gmail.com>
2020-01-31 17:16:27 +0000

    feat: added evenListener

---
+++ contract.js
@@ -0,0 +1,9 @@
+const Orientation = {
+  allowedOrientationsForScreen: () => null,
+  releaseOrientationsForScreen: () => null,
+  portrait: 1,
+  landscapeRight: 2,
+  landscapeLeft: 4,
+  portraitUpsideDown: 8,
+  landscapeSensor: landscapeRight + landscapeLeft,
+};
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { sendQuickBrickEvent } from "@applicaster/zapp-react-native-bridge/QuickBrick";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, DeviceEventEmitter } from "react-native";
+import {
+  sendQuickBrickEvent,
+  NativeEventEmitter,
+} from "@applicaster/zapp-react-native-bridge/QuickBrick";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -8,17 +12,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "black",
     width: "100%",
-    height: "100%"
+    height: "100%",
   },
   text: {
     color: "white",
-    fontSize: 25
-  }
+    fontSize: 25,
+  },
 });
 const App = () => {
   sendQuickBrickEvent("allowedOrientationsForScreen", {
-    orientation: 1
+    orientation: 1,
   });
+  useEffect(() => {
+    DeviceEventEmitter.addListener("orientationChange", res =>
+      console.log("Callback:", res)
+    );
+    return DeviceEventEmitter.removeListener("orientationChange");
+  }, []);
   console.log("it works!");
   return (
     <View style={styles.container}>
